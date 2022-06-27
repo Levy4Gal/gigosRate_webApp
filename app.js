3ab5e69d742de4d84eeb563884f5338c1cf1a6ab
shaharkozi <102664929+shaharkozi@users.noreply.github.com>
2022-06-27 17:08:39 +0300

    handle login

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 const app = express();
 const path = require("path");
 const port = 8080;
@@ -6,7 +8,21 @@ const port = 8080;
 app.use(express.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/views/index.html"));
+  res.sendFile(path.join(__dirname, "public/views/SignIn"));
 });
 
-app.listen(port, () => console.info("Listening on port " % { port }));
+app.post("")
+console.log("here");
+app.listen(port, () => console.info("Listening on port ", port ));
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Shaharkozi:S123456@gigos.kdk9a.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+router.post("/login",(req, res) => {
+  var user_name = req.body.user;
+  var password = req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
+  });
