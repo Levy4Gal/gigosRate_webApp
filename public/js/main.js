82be2848ab0b4cf1a3008b02e28434cb17ac4336
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-07-05 18:55:09 +0300

    changed folders

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  var el = document.getElementById("content1");
+  el.innerHTML =
+    "<html><div class ='movieDiv'> <img class='img' src='img/casino-royale.jpg' /><img class='img' src='img/casino-royale.jpg' /></div></html>";
+});
