b602c2323985b0fd2745d6e8af1826a87be29375
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-08-12 13:42:49 +0300

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
-  var el = document.getElementById("content1");
-  el.innerHTML =
-    "<html><div class ='movieDiv'> <img class='img' src='img/casino-royale.jpg' /><img class='img' src='img/casino-royale.jpg' /></div></html>";
+  var e = document.getElementById("content");
+  var e1 = document.createElement("div");
+  var e2 = document.createElement("div");
+  e.appendChild(e1);
+  e.appendChild(e2);
+  let img = "<img class='img' src='img/casino-royale.jpg' />";
+  let img1 = "<img class='img' src='img/action.jpg' />";
+  let text = "<p class = 'text' >Casino Royal</p>";
+
+  let movie = "<span class ='movieDiv'>" + img + text + "</span>";
+  let movie1 = "<span class ='movieDiv'>" + img1 + text + "</span>";
+
+  e1.innerHTML =
+    "<html><div class ='movieRow'>" +
+    movie +
+    movie +
+    movie +
+    movie +
+    "</div></html>";
+
+  e2.innerHTML =
+    "<html><div class ='movieRow'>" +
+    movie1 +
+    movie1 +
+    movie1 +
+    movie1 +
+    "</div></html>";
+
+  //var el2 = document.getElementById("content2");
+  // el.innerHTML = "<html><div class ='movieDiv'>" + img + "</div></html>";
 });
