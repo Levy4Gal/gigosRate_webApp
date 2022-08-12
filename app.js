b9cc946840b1682ee96d4b6d36ca7aed72c45b46
OrBenNun <obn2468@gmail.com>
2022-08-12 14:46:38 +0300

    commit

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 8080;
 
 app.use(express.json());
 app.use(express.static("public"));
+
+app.set('views','./public/views');
 // app.use(express.static("public/views"))
 // app.set('views','./public/views');
 app.get("/", (req, res) => {
@@ -12,3 +14,20 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => console.info("Listening on port " ,port));
+
+app.get("/sign_up", (req, res) => {
+  // res.sendFile(path.join(__dirname, "public/views/index.html"));
+  // res.sendFile(path.join(__dirname, "public/views/signup2.html"));
+  // getSignUpPage();
+});
+
+
+function getSignUpPage(){
+  console.log("inside signup load");
+  document.getElementsByClassName("contect").load(__dirname +"public/views/SignUp.html");
+  // $(".content").load(__dirname +"public/views/SignUp.html");  
+}
+
+
+
+
---
+++ public/index.js
@@ -0,0 +1,11 @@
+function SignUpPage(){
+    $(document).ready(function(){
+        $(".content").load("views/SignUp.html"); 
+    });
+    
+    // $(".content").load("views/SignUp.html"); 
+}
+
+function SignInPage(){
+    $(".content").load("views/signIn.html"); 
+}
