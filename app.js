e25fc4523bd89c36e03eb5e02efb5d30db9ad4ef
shaharkozi <102664929+shaharkozi@users.noreply.github.com>
2022-08-12 15:12:59 +0300

    commit1

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = express.Router();
 const app = express();
 const path = require("path");
 const port = 8080;
+var hash = require('object-hash');
 
 app.use(express.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/views/SignIn"));
+  res.sendFile(path.join(__dirname, "public/views/SignIn.html"));
 });
 
 app.post("")
-console.log("here");
 app.listen(port, () => console.info("Listening on port ", port ));
 
 
@@ -20,9 +19,49 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://Shaharkozi:S123456@gigos.kdk9a.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-router.post("/login",(req, res) => {
+app.post("/login",(req, res) => {
   var user_name = req.body.user;
   var password = req.body.password;
+  console.log(req.query);
   console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
   });
+
+  //-----------DataBase Functions------------//
+
+  function addUser(userName, password, isAdmin){
+    client.connect( err => {
+      const users = client.db("gigos").collection("users");
+      var pass = hash(password);
+      const doc = {
+        "userName":userName,
+        "password": pass,
+        "isAdmin":isAdmin
+      }
+      users.insertOne(doc);
+      setTimeout(()=> client.close(),1500);
+    });
+  }
+  // addUser("Guy","Guy");
+  async function autheticateUser(userName, password ,callback) {
+    try {
+      client.connect(async err => {
+        const users = client.db("gigos").collection("users");
+        var pass = hash(password);
+        const doc = {
+          "userName":userName,
+          "password": pass,
+        }
+        const res =await users.findOne(doc);
+        callback(res);
+        setTimeout(()=> client.close(),1500);
+      });
+    } finally {
+      await client.close();
+    }
+  }
+  function test(value){
+    console.log(value);
+  }
+  autheticateUser("Guy","Guy", test);
+
