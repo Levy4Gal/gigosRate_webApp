afd1a04869c9e35f4b12311b602025984a2ce49e
inon angel <inon.angel412@gmail.com>
2022-08-12 13:51:08 +0300

    admin nodejs update

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const port = 8080;
 
 app.use(express.json());
 app.use(express.static("public"));
+
 // app.use(express.static("public/views"))
 // app.set('views','./public/views');
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/views/index.html"));
 });
+
+//Admin test
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/views/admin.html"));
+});
+
 
 app.listen(port, () => console.info("Listening on port " ,port));
