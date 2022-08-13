2bc4e4ec8693cc77ccc08159a1217b9a0706db87
shaharkozi <102664929+shaharkozi@users.noreply.github.com>
2022-08-13 16:29:00 +0300

    DB functions

--- app.js
+++ app.js
@@ -27,9 +27,17 @@ app.post("/login",(req, res) => {
   res.end("yes");
   });
 
+  //Admin test
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/views/admin.html"));
+});
+
+
+app.listen(port, () => console.info("Listening on port " ,port));
+
   //-----------DataBase Functions------------//
 
-  function addUser(userName, password, isAdmin){
+  function createUser(userName, password, isAdmin, res){//helper function for signup
       const users = client.db("gigos").collection("users");
       var pass = hash(password);
       const doc = {
@@ -37,8 +45,16 @@ app.post("/login",(req, res) => {
         "password": pass,
         "isAdmin":isAdmin
       }
-      users.insertOne(doc);
-      setTimeout(()=> client.close(),1500);
+      const valid = {
+        "userName":userName}
+      users.findOne(valid, function(err, result){
+        if(err) throw err;
+        console.log(result);
+        if(result != null){
+          res.send("this user name is not available")
+        }else 
+          users.insertOne(doc);
+        });
   }
 
    function autheticateUser(userName, password ,res) {//pull thr data from DB for the /get/authenticateUsre
@@ -76,22 +92,129 @@ app.post("/login",(req, res) => {
         res.send(result)});
  }
 
+ function addMovie( userName, movieName, description,locations,trailer,rate,duration,director,stars,img, res){//helper function for signup
+  const users = client.db("gigos").collection("users");
+  const movies = client.db("gigos").collection("movies");
+  const user = {
+    "userName":userName,
+  }
+  const movie = {
+    "movieName": movieName,
+    "description": description,
+    "locations": locations,
+    "trailer": trailer,
+    "rate": rate,
+    "duration": duration,
+    "director": director,
+    "stars": stars,
+    "img": img
+}
+  users.findOne(user, function(err, result){
+    if(err) throw err;
+    if(result.isAdmin){
+      movies.findOne(movie, function(err, result){
+        if(err) throw err;
+        if(result != null){
+          res.send("this movie is already exist")
+        }else 
+          movies.insertOne(movie);
+        });
+    }
+    else
+      return null;});
+}
+
+function getMovie( movieName, res){//helper function for signup
+  const movies = client.db("gigos").collection("movies");
+  const movie = {
+    "movieName": movieName,
+}
+    movies.findOne(movie, function(err, result){
+      if(err) throw err;
+      if(result != null){
+        res.send(result)
+      }else 
+        res.send("this movie is not exist");
+      });
+}
+
+async function getAllMovies(res){//helper function for signup
+  var movies = await client.db("gigos").collection("movies").find().toArray();
+  res.send(movies);
+}
+
+async function getFirstsMovies(num, res){//helper function for signup
+  var movies = await client.db("gigos").collection("movies").find().limit(Number.parseInt(num)).toArray();
+  res.send(movies);
+}
+
+function rate(movieName,userName, newRate){//functionality of update rate
+  const movies = client.db("gigos").collection("movies");
+  const movie = {
+    "movieName": movieName,
+}
+  movies.findOne(movie, async function(err, result){//find the movie that we want to update his rate
+    if(err) throw err;
+    if(result != null){//if the movie exist
+      var jsonRate = JSON.parse(result.rate);//take the json of the rate value
+      var tRate = 0;//calculate the new total rate
+      var i = 0;
+      for (var key of Object.keys(jsonRate)) {//iterate over all the rating values
+        if(key != "totalRate"){
+          i++;
+          tRate += Number.parseFloat(jsonRate[key]);
+        }
+      }
+      i++;
+      tRate += Number.parseFloat(newRate);
+      tRate /= i;
+      jsonRate["totalRate"] = tRate;//assign the new totalRate to the json
+      jsonRate[userName] = newRate;//append the new rating to json
+      result.rate = JSON.stringify(jsonRate);
+      var movie = {
+        "movieName" : movieName
+      };
+      var newValues = {$set: result};
+      client.db("gigos").collection("movies").updateOne(movie,newValues ,function(err, res) {//update the values at the DB
+        if (err) throw err;
+        console.log("1 document updated");
+      });
+    }else 
+      return//if the movie dont exist return
+    });
+}
+
+ app.post("/signUp",(req, res) => {//req  parameters:  userName and password and isAdmin. if the user name is already exist return res = "this user name is not available"
+  createUser(req.query.userName,req.query.password, req.query.isAdmin, res);
+});
+
 
 app.get("/authenticateUsre", (req, res) => {//get the parameters:  userName and password. return Json key: "isExist": value:false/true
-  console.log(req.query.userName);
-  console.log(req.query.password);
   autheticateUser(req.query.userName,req.query.password, res);
 });
 
-app.get("/user", (req, res) => {//get the parameters:  userName and password. return Json key: "isExist": value:false/true
-  console.log(req.query.userName);
+app.get("/user", (req, res) => {//get the parameters:  userName . return Json of the user, if user is not exist return this Json {     key:"userName": value:"this user is not exist" }
   getUser(req.query.userName, res);
 });
 
-//Admin test
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/views/admin.html"));
+app.post("/addMovie",(req, res) => {//req  parameters:  user name, movieName, description, locations, trailer, rate,duration, director, stars, img. if the movie is already exist return res = "this movie is already exist", case that the user is not admin return none
+  addMovie(req.query.userName ,req.query.movieName, req.query.description,
+    req.query.locations,req.query.trailer,req.query.rate,req.query.duration,req.query.director,req.query.stars,req.query.img, res);
 });
 
+app.get("/movie", (req, res) => {//req  parameters:  movieName. if the movie is not exist return res = "this movie is not exist", else return the movie
+  getMovie(req.query.movieName, res);
+});
 
-app.listen(port, () => console.info("Listening on port " ,port));
+app.get("/allMovies", (req, res) => {//req  parameters:  none
+  getAllMovies(res);
+});
+
+app.get("/firstMovies", (req, res) => {//req  parameters:  number of the firsts
+  getFirstsMovies(req.query.num ,res);
+});
+
+app.post("/rate", (req, res) => {//req  parameters:  movieName, usreName, rate
+  rate(req.query.movieName, req.query.userName, req.query.rate ,res);
+});
+
